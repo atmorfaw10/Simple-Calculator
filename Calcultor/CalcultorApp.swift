a59b98e18a33159ab74240e2b6afb0a50c207a5a
atmorfaw10 <asongtmorfaw_sd10@users.noreply.github.com>
2021-04-14 23:52:27 -0400

    Initial Commit

---
+++ Calcultor/CalcultorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcultorApp.swift
+//  Calcultor
+//
+//  Created by Asong Morfaw on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct CalcultorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
